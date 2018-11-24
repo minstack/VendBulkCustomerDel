20fa49bc552f0801e03c249eced50f3a03b94063
minstack <sungminyoon@Sungs-MacBook-Air.local>
2018-11-23 21:06:45 -0500

    First commit. Basic VendApi class.

---
+++ VendApi.py
@@ -0,0 +1,30 @@
+import requests
+
+class VendApi:
+
+    __BASE_URL = "https://{0}.vendhq.com/"
+    __ENDPOINTS = {
+        "cust" : "api/2.0/customers",
+        "search" : "api/2.0/search",
+        "sales" : "api/2.0/sales"
+    }
+
+    __domain = ''
+    __headers = {"Authorization" : ""}
+
+    def __init__(self, prefix, token):
+        self.__domain = self.__BASE_URL.format(prefix)
+        self.__headers["Authorization"] = "Bearer " + token
+
+
+    def getCustomers(self):
+        return self.__getRequest__(self, self.__domain + self.__ENDPOINTS['cust'])
+
+
+    def __getRequest__(self, url):
+        response = requests.request("GET", url, headers = self.__headers)
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()['data']
